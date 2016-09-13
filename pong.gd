d2dac953738daf997ba93283f3ce913b5683ff5e
Cristian Morataya <cris.morataya@gmail.com>
2016-09-13 13:54:13 -0600

    storing sizes of the screen and the left pad

--- pong.gd
+++ pong.gd
@@ -1,7 +1,13 @@
 
 extends Node2D
 
+var screen_size
+var pad_size
+
 func _ready():
+	screen_size = get_viewport_rect().size
+	pad_size = get_node("left").get_texture().get_size()
+	set_process(true)
 	pass
 
 
